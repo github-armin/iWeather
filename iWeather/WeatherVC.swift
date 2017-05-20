ec5995a72556bdd964a07c28d1f5e32fefd435de
Armin <kzardkoohi@gmail.com>
2017-05-20 04:10:13 -0500

    created custom cell UI. added code in WeatherVC for cells. still need to create custom cell class.

--- iWeather/WeatherVC.swift
+++ iWeather/WeatherVC.swift
@@ -8,15 +8,43 @@
 
 import UIKit
 
-class WeatherVC: UIViewController {
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var todayDateLbl:UILabel!
+    
+    @IBOutlet weak var currentTempLbl: UILabel!
+    
+    @IBOutlet weak var locationLbl: UILabel!
+    
+    @IBOutlet weak var currentWeatherLbl: UILabel!
+    
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
         
         
     }
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath)
+        return cell
+    }
 
 
 }
