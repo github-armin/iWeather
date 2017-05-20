b83076e92bb749908cc966e325d89172d72fa08f
Armin <kzardkoohi@gmail.com>
2017-05-20 14:32:07 -0500

    got tableviewcells working

---
+++ iWeather/WeatherData.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherData.swift
+//  iWeather
+//
+//  Created by Kevin Armin Zardkoohi on 5/20/17.
+//  Copyright © 2017 BDA. All rights reserved.
+//
+
+import Foundation
+
+class WeatherData {
+    
+}
