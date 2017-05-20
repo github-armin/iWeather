5dffa2c4bfc345ca463798365854efb6647625f4
Armin <kzardkoohi@gmail.com>
2017-05-20 03:27:08 -0500

    added alamofire podfile & config. changed viewcontroller file/class name and added reference to UI. updated UI.

---
+++ iWeather/WeatherVC.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherVC.swift
+//  iWeather
+//
+//  Created by Kevin Armin Zardkoohi on 5/19/17.
+//  Copyright © 2017 BDA. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+
+}
+
